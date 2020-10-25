3fcafcb7a89311c3bfc541a8e8942f88c8e3bc45
Decio <decio.msoares@gmail.com>
2020-10-25 15:24:42 +0000

    add Procfile

--- index.js
+++ index.js
@@ -120,5 +120,5 @@ app.post('/api/persons', (req, res) => {
 
 });
 
-const PORT = '3001';
+const PORT = process.env.PORT || '3001';
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
